94c7dfa6b90082b90c8efd88e2fa178ef28ab47b
Natanael Copa <ncopa@alpinelinux.org>
2011-09-30 11:56:43 +0000

    aports.lua: try get vars from env var before parsing abuild.conf

--- aports.lua
+++ aports.lua
@@ -5,11 +5,21 @@ abuild_conf_file = "/etc/abuild.conf"
 local abuild_conf = {}
 
 function get_abuild_conf(var)
-	if abuild_conf[var] == nil then
-		local f = io.popen(" . "..abuild_conf_file..' ; echo -n "$'..var..'"')
-		abuild_conf[var] = f:read("*all")
-		f:close()
+	-- check cache
+	if abuild_conf[var] ~= nil then
+		return abuild_conf[var]
 	end
+
+	-- use os env var
+	abuild_conf[var] = os.getenv(var)
+	if abuild_conf[var] ~= nil then
+		return abuild_conf[var]
+	end
+
+	-- parse config file
+	local f = io.popen(" . "..abuild_conf_file..' ; echo -n "$'..var..'"')
+	abuild_conf[var] = f:read("*all")
+	f:close()
 	return abuild_conf[var]
 end
 
