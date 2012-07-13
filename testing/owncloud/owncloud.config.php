1008ddd5bc8e9b76833695565c6733a3f796e79c
Leonardo Arena <rnalrd@alpinelinux.org>
2012-07-13 07:47:14 +0000

    testing/owncloud: provide a basic default config file

---
+++ testing/owncloud/owncloud.config.php
@@ -0,0 +1,11 @@
+<?php
+$CONFIG = array(
+"datadirectory" => '/var/lib/owncloud/data',
+"version" => '4.0.4',
+"dbname" => 'owncloud',
+"dbhost" => 'localhost',
+"dbuser" => 'owncloud',
+"installed" => false,
+"loglevel" => '0',
+);
+?>
