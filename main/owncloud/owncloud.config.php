998e80b0319a92a27a451eb3c8650462bcc079e6
Leonardo Arena <rnalrd@alpinelinux.org>
2013-03-14 08:25:54 +0000

    main/owncloud: upgrade to 4.5.8

--- main/owncloud/owncloud.config.php
+++ main/owncloud/owncloud.config.php
@@ -1,7 +1,7 @@
 <?php
 $CONFIG = array(
 "datadirectory" => '/var/lib/owncloud/data',
-"version" => '4.5.7',
+"version" => '4.5.8',
 "dbname" => 'owncloud',
 "dbhost" => 'localhost',
 "dbuser" => 'owncloud',
