e1339a9b7754ad1dd1c01b3cdf87ea340c58b461
Dubiousjim <dubiousjim@gmail.com>
2013-07-05 00:21:12 -0400

    various: add descriptions, attribution, license

--- abuild-sudo.c
+++ abuild-sudo.c
@@ -1,3 +1,12 @@
+/* abuild-sudo.c - limited root privileges for users in "abuild" group
+ *
+ * Copyright (C) 2012 Natanael Copa <ncopa@alpinelinux.org>
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation. See http://www.gnu.org/ for details.
+ */
 
 #include <sys/types.h>
 
