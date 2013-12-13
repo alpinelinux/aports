b80c7cb08ed14fa8bdcd7fe967599f16d7ebf9ab
Leonardo Arena <rnalrd@alpinelinux.org>
2013-12-13 15:26:26 +0000

    main/owncloud: upgrade to 6.0.0

--- main/owncloud/owncloud.config.php
+++ main/owncloud/owncloud.config.php
@@ -1,7 +1,7 @@
 <?php
 $CONFIG = array(
 "datadirectory" => '/var/lib/owncloud/data',
-"version" => '5.0.12',
+"version" => '6.0.0',
 "dbname" => 'owncloud',
 "dbhost" => 'localhost',
 "dbuser" => 'owncloud',
