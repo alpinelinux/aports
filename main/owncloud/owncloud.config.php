2311d50368435ae4847c3899733d89d91dd9f51c
Leonardo Arena <rnalrd@alpinelinux.org>
2013-12-18 13:58:05 +0000

    main/owncloud: upgrade to 5.0.14a

--- main/owncloud/owncloud.config.php
+++ main/owncloud/owncloud.config.php
@@ -1,7 +1,7 @@
 <?php
 $CONFIG = array(
 "datadirectory" => '/var/lib/owncloud/data',
-"version" => '5.0.12',
+"version" => '5.0.14a',
 "dbname" => 'owncloud',
 "dbhost" => 'localhost',
 "dbuser" => 'owncloud',
