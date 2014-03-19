436abe5bafbe71dfb62e2eb0d187c4a9a3749574
Leonardo Arena <rnalrd@alpinelinux.org>
2014-03-19 14:33:12 +0000

    main/owncloud: use www-data group, make default apps dir read-only, workaround locale check

--- main/owncloud/owncloud.config.php
+++ main/owncloud/owncloud.config.php
@@ -1,11 +1,25 @@
 <?php
 $CONFIG = array(
-"datadirectory" => '/var/lib/owncloud/data',
-"version" => '6.0.0',
-"dbname" => 'owncloud',
-"dbhost" => 'localhost',
-"dbuser" => 'owncloud',
-"installed" => false,
-"loglevel" => '0',
+  'datadirectory' => '/var/lib/owncloud/data',
+  'apps_path' => array (
+    0 => array (
+      "path" => OC::$SERVERROOT."/apps",
+      "url" => "/apps",
+      "writable" => false,
+    ),
+    /* Uncomment this and install your apps here
+    1 => array (
+      "path" => "/var/www/localhost/htdocs/myoc_apps",
+      "url" => "/myoc_apps",
+      "writable" = true,
+    ),
+    */
+  ),
+  'version' => '6.0.2',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbuser' => 'owncloud',
+  'installed' => false,
+  'loglevel' => '0',
 );
 ?>
