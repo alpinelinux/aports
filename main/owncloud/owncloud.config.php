d35b9352d16523a40d30e2e34e608aadefd0efb6
Leonardo Arena <rnalrd@alpinelinux.org>
2014-07-01 06:24:18 +0000

    main/owncloud: security upgrade to 5.0.17

--- main/owncloud/owncloud.config.php
+++ main/owncloud/owncloud.config.php
@@ -1,7 +1,7 @@
 <?php
 $CONFIG = array(
 "datadirectory" => '/var/lib/owncloud/data',
-"version" => '5.0.14a',
+"version" => '5.0.17',
 "dbname" => 'owncloud',
 "dbhost" => 'localhost',
 "dbuser" => 'owncloud',
