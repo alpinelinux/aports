8af1bb617f41a9599108b52a123d1e3abadd3578
Natanael Copa <ncopa@alpinelinux.org>
2015-10-02 10:25:16 +0200

    main/aports-build: report build errors to mqtt via hook

---
+++ main/aports-build/report-build-errors.lua
@@ -0,0 +1,29 @@
+local publish = require("mqtt.publish")
+local json = require("cjson")
+
+local hostname
+local f = io.open("/proc/sys/kernel/hostname")
+hostname = f:read()
+f:close()
+
+local urlprefix=("http://build.alpinelinux.org/buildlogs/%s"):format(hostname)
+
+local m = {}
+
+function m.postbuild(aport, success, repodest, arch, logfile)
+	if not success then
+		local topic = ("build/%s/errors"):format(hostname)
+		local payload =	json.encode{
+			pkgname = aport.pkgname,
+			logurl = ("%s/%s/%s-%s-r%s.log"):format(
+					urlprefix,
+					(string.match(aport.dir,"[^/]+/[^/]+$")),
+					aport.pkgname, aport.pkgver, aport.pkgrel),
+			status = success
+		}
+		publish.single(topic, payload, nil, nil, "msg.alpinelinux.org")
+	end
+end
+
+return m
+
