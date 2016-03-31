367991275b8f37b8bdd024eceddbb4c2bb981f76
Natanael Copa <ncopa@alpinelinux.org>
2016-03-31 09:57:22 +0000

    community/firefox-esr: upgrade to 45.0.1

--- community/firefox-esr/vendor.js
+++ community/firefox-esr/vendor.js
@@ -1,3 +1,5 @@
 // Disable default browser checking.
 pref("browser.shell.checkDefaultBrowser", false);
+// OMTC and system cairo is broken
+pref("layers.offmainthreadcomposition.enabled", false);
 
