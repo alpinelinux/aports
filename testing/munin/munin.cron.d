ddac33d5ae01adc25937c3c9f54246e5b36bbb33
Natanael Copa <ncopa@alpinelinux.org>
2016-04-15 06:52:53 +0000

    Revert "testing/munin: new aport"
    This reverts commit 8050ff6cc71bdb64f6c2a745cc127178f6502956.

--- testing/munin/munin.cron.d
+++
@@ -1,9 +0,0 @@
-# /etc/cron.d/munin: crontab entries for the munin package
-
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-MAILTO=root
-
-@reboot         munin if [ -x /usr/bin/munin-cron ]; then /usr/bin/munin-cron; fi
-*/5 * * * *     munin if [ -x /usr/bin/munin-cron ]; then /usr/bin/munin-cron; fi
-
-# EOF
