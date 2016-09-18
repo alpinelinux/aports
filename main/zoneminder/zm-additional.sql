ef4cdc2edd6de8a29bb2f8b021e83858f62bb3c5
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2016-09-18 18:55:13 +0300

    main/zoneminder: upgrade to 1.30.0

--- main/zoneminder/zm-additional.sql
+++
@@ -1,4 +0,0 @@
-# Update settings to reflect alpine linux default install
-update Config set Value = '/cgi-bin/zm/nph-zms' where Name = 'ZM_PATH_ZMS';
-update Config set Value = '-5' where Name = 'ZM_LOG_LEVEL_SYSLOG';
-update Config set Value = '0' where Name = 'ZM_LOG_LEVEL_FILE';
