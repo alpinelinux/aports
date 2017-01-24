e46a2aa534cb95a4796c2dbcef0e2b1a9347bd62
Fabiano Rosas <farosas@linux.vnet.ibm.com>
2017-01-24 15:39:10 -0200

    Add ppc64le to minirootfs arches

--- scripts/mkimg.minirootfs.sh
+++ scripts/mkimg.minirootfs.sh
@@ -16,6 +16,6 @@ create_image_rootfs() {
 profile_minirootfs() {
 	image_ext=tar.gz
 	output_format=rootfs
-	arch="x86 x86_64 armhf aarch64"
+	arch="x86 x86_64 armhf aarch64 ppc64le"
 	rootfs_apks="busybox alpine-baselayout alpine-keys apk-tools libc-utils"
 }
