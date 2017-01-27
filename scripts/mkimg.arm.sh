3301f083c6c354cba8693e596ace2e4f5ff0247c
Timo Teräs <timo.teras@iki.fi>
2017-01-27 13:42:06 +0200

    scripts/mkimg: raspberry pi bootloader 2017-01-25

--- scripts/mkimg.arm.sh
+++ scripts/mkimg.arm.sh
@@ -48,7 +48,7 @@ profile_rpi() {
 	profile_base
 	image_ext="tar.gz"
 	arch="armhf"
-	rpi_firmware_commit="4bf906cdd221c4f6815d0da7dda0cd59d25d945b"
+	rpi_firmware_commit="debe2d29bbc3df84f74672fae47f3a52fd0d40f1"
 	kernel_flavors="rpi rpi2"
 	kernel_cmdline="dwc_otg.lpm_enable=0 console=ttyAMA0,115200 console=tty1"
 	initrd_features="base bootchart squashfs ext2 ext3 ext4 f2fs kms mmc raid scsi usb"
