8cde8e623c82c09a5a2fbb29005e80d97e126601
Chris A. Giorgi <chrisgiorgi@gmail.com>
2017-03-09 10:22:02 -0800

    xtables-addons - Only include the -grsec flavor, not all of them.

--- scripts/mkimage/features/xtables-addons/feature-xtables-addons.sh
+++ scripts/mkimage/features/xtables-addons/feature-xtables-addons.sh
@@ -2,9 +2,9 @@
 
 feature_xtables_addons() {
 	if kernel_flavors_has "grsec" ; then
-		add_initfs_apks_flavored "xtables-addons"
+		add_initfs_apks "xtables-addons-grsec"
 		add_apks "xtables-addons"
 	else
-		echo "WARNING: Feature_xtables_addons is not currently supported any of your enabled kernel flavor ($(get_kernel_flavors))."
+		echo "WARNING: Feature_xtables_addons is not currently supported any of your enabled kernel flavors ($(get_kernel_flavors))."
 	fi
 }
