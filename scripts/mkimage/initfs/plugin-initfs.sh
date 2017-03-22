ecff28add94fa9b252a07d24535965f3dae7d607
Chris A. Giorgi <chrisgiorgi@gmail.com>
2017-03-21 20:59:44 -0700

    Moved plugin_initfs functions from plugin-kernel to plugin-initfs.

--- scripts/mkimage/initfs/plugin-initfs.sh
+++ scripts/mkimage/initfs/plugin-initfs.sh
@@ -1,5 +1,26 @@
 plugin_initfss() {
 	var_list_alias initfs_files
 	var_list_alias initfs_modules
+
+	var_list_alias initfs_features
+	var_list_alias initfs_load_modules
+
+	var_list_alias initfs_apks
+	var_list_alias initfs_apks_flavored
+	var_list_alias initfs_only_apks
+	var_list_alias initfs_only_apks_flavored
+
+	# initfs_cmdline
+	return 0
+}
+
+
+get_initfs_cmdline() {
+	_mod="$(get_initfs_load_modules_with_sep ',')"
+	printf '%s' "${_mod:+modules=$_mod}${initfs_cmdline:+ $initfs_cmdline}"
+}
+
+append_initfs_cmdline() {
+	initfs_cmdline="${initfs_cmdline}${1:+ $1}"
 }
 
--- scripts/mkimage/kernels/plugin-kernels.sh
+++ scripts/mkimage/kernels/plugin-kernels.sh
@@ -1,28 +1,3 @@
-plugin_initfs() {
-	# initfs_cmdline
-	# 
-	var_list_alias initfs_features
-	var_list_alias initfs_load_modules
-
-	var_list_alias initfs_apks
-	var_list_alias initfs_apks_flavored
-	var_list_alias initfs_only_apks
-	var_list_alias initfs_only_apks_flavored
-
-	return 0
-}
-
-
-get_initfs_cmdline() {
-	# Intentionally not quoted to clean up list!
-	_mod="$(get_initfs_load_modules_with_sep ',')"
-	printf '%s' "${_mod:+modules=$_mod}${initfs_cmdline:+ $initfs_cmdline}"
-}
-
-append_initfs_cmdline() {
-	initfs_cmdline="${initfs_cmdline}${1:+ $1}"
-}
-
 
 
 plugin_kernels() {
