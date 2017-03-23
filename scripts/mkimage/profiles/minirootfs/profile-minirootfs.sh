e41c9f88d15572c5371cc68bb4bf95d0ce90ed59
Chris A. Giorgi <chrisgiorgi@gmail.com>
2017-03-23 14:20:06 -0700

    profle-minirootfs - Remove unneeded explicit 'busybox' from rootfs_apks. (pickfire)

--- scripts/mkimage/profiles/minirootfs/profile-minirootfs.sh
+++ scripts/mkimage/profiles/minirootfs/profile-minirootfs.sh
@@ -6,5 +6,5 @@ profile_minirootfs() {
 	set_image_ext tar.gz
 	set_output_format rootfs
 	set_archs "x86 x86_64 armhf aarch64 ppc64le"
-	set_rootfs_apks "busybox alpine-baselayout alpine-keys apk-tools libc-utils"
+	set_rootfs_apks "alpine-baselayout alpine-keys apk-tools libc-utils"
 }
