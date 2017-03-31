0cd30d353300606b5c91cfae2dc85259e05268dd
Chris A. Giorgi <chrisgiorgi@gmail.com>
2017-03-30 19:54:16 -0700

    Add mkinitfs feature converter initfs/initfs-convert-feature.sh. Accepts path to old feature (with or without extension) and generates new sh style feature to stdout.

---
+++ scripts/mkimage/initfs/initfs-convert-feature.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Converts old <feature>.file and <feature>.module mkinitfs feature to new sh based format.
+# Usage: initfs-convert-feature.sh <feature directory>/<feature basename>[.<(module|files)>]
+
+for p ; do
+f="${p##*/}" && f="${f%.*}"
+d="${p%/*}"
+
+
+cat <<EOF
+# Initifs feature $f.
+
+initfs_$f() {
+	return 0
+}
+EOF
+
+cat << EOF
+
+_initfs_$f_modules() {
+$( if [ -e "${d}/$f.modules" ] ; then
+	printf "\tcat <<-'EOF'\n"
+	cat "${d}/$f.modules" | sed -e 's|^|\t\t|g'
+	printf "\tEOF\n"
+   else printf '\treturn 0\n' ; fi
+)
+}
+EOF
+
+cat << EOF
+
+_initfs_$f_files() {
+$( if [ -e "${d}/$f.files" ] ; then
+	printf "\tcat <<-'EOF'\n"
+	cat "${d}/$f.files" | sed -e 's|^|\t\t|g'
+	printf "\tEOF\n"
+   else printf '\treturn 0\n' ; fi
+)
+}
+EOF
+
+done
