419e51638c715d8fdfa4fab1ad40bec66deb92b9
Jakub Jirutka <jakub@jirutka.cz>
2017-06-10 15:02:51 +0200

    test: fix test6.sh, replace linux-grsec w/ linux-vanilla, add alpine-keys

--- test/test6.sh
+++ test/test6.sh
@@ -3,9 +3,9 @@
 # desc: test triggers in kernel package
 
 $APK add --root $ROOT --initdb -U --repository $PWD/repo1 \
-	--repository $SYSREPO linux-grsec
+	--repository $SYSREPO alpine-keys linux-vanilla
 
-test -L "$ROOT"/boot/vmlinuz-grsec
+test -L "$ROOT"/boot/vmlinuz
 
-test -L "$ROOT"/boot/initramfs-grsec
+test -L "$ROOT"/boot/initramfs-vanilla
 
