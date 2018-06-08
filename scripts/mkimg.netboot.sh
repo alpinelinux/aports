c63d504e458fe9cdd60ac484531693fa83b08b50
Natanael Copa <ncopa@alpinelinux.org>
2018-06-08 14:06:15 +0000

    scripts/mkimage: add netboot profile

---
+++ scripts/mkimg.netboot.sh
@@ -0,0 +1,23 @@
+create_image_netboot() {
+	rm -rf "${OUTDIR}"/netboot-$RELEASE
+	cp -a "${DESTDIR}"/boot "${OUTDIR}"/netboot-$RELEASE
+	tar -C "${DESTDIR}" -chzf ${OUTDIR}/${output_filename} boot/
+}
+
+profile_netboot() {
+	title="Netboot"
+	desc="Kernel, initramfs and modloop for
+		netboot.
+		"
+	arch="x86_64 s390x"
+	kernel_cmdline="nomodeset"
+	kernel_flavors="vanilla"
+	apks=""
+	initfs_features="ata base bootchart squashfs ext2 ext3 ext4 mmc network scsi usb virtio"
+	output_format="netboot"
+	image_ext="tar.gz"
+	case "$ARCH" in
+	x86_64) kernel_flavors="$kernel_flavors virt";;
+	esac
+}
+
