20bae8ec3098ed6c39d38458c3d69c923b893cd0
Natanael Copa <ncopa@alpinelinux.org>
2018-06-12 08:02:45 +0000

    scripts/mkimg.netboot.sh: tweak initramfs, add aarch64, fix copy

--- scripts/mkimg.netboot.sh
+++ scripts/mkimg.netboot.sh
@@ -1,6 +1,6 @@
 create_image_netboot() {
 	rm -rf "${OUTDIR}"/netboot-$RELEASE
-	cp -a "${DESTDIR}"/boot "${OUTDIR}"/netboot-$RELEASE
+	cp -aL "${DESTDIR}"/boot "${OUTDIR}"/netboot-$RELEASE
 	tar -C "${DESTDIR}" -chzf ${OUTDIR}/${output_filename} boot/
 }
 
@@ -9,11 +9,11 @@ profile_netboot() {
 	desc="Kernel, initramfs and modloop for
 		netboot.
 		"
-	arch="x86_64 s390x"
+	arch="aarch64 x86_64 s390x"
 	kernel_cmdline="nomodeset"
 	kernel_flavors="vanilla"
 	apks=""
-	initfs_features="ata base bootchart squashfs ext2 ext3 ext4 mmc network scsi usb virtio"
+	initfs_features="base network squashfs usb virtio"
 	output_format="netboot"
 	image_ext="tar.gz"
 	case "$ARCH" in
