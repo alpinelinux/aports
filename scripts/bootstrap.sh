a0e3ae1279815d5ed5ce4c45f681463148219c60
Timo Teräs <timo.teras@iki.fi>
2018-08-02 08:18:55 +0000

    main/busybox, scripts/bootstrap: fix bootstrap
    busybox now requires ssl on the target host. Fix busybox dependency
    and adjust bootstrap build order accordingly.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -96,8 +96,8 @@ apk info --quiet --installed --root "$CBUILDROOT" libgcc libstdc++ musl-dev || \
 
 # ordered cross-build
 for PKG in fortify-headers linux-headers musl libc-dev pkgconf zlib \
-	   busybox busybox-initscripts binutils make \
-	   libressl apk-tools file \
+	   libressl busybox busybox-initscripts binutils make \
+	   apk-tools file \
 	   gmp mpfr3 mpc1 isl cloog gcc \
 	   openrc alpine-conf alpine-baselayout alpine-keys alpine-base build-base \
 	   attr libcap patch sudo acl fakeroot tar \
