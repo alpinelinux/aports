44b9028139227a4964e7a3457c82a5ce1cc8e68e
Carlo Landmeter <clandmeter@alpinelinux.org>
2019-01-28 20:36:02 +0000

    mkimage: netboot sign modloop

--- scripts/mkimg.netboot.sh
+++ scripts/mkimg.netboot.sh
@@ -13,6 +13,7 @@ profile_netboot() {
 	arch="aarch64 armhf ppc64le x86 x86_64 s390x"
 	kernel_cmdline="nomodeset"
 	kernel_flavors="vanilla"
+	modloop_sign=yes
 	apks=""
 	initfs_features="base network squashfs usb virtio"
 	output_format="netboot"
