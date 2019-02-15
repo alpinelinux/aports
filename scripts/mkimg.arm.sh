680b40d3d2eebb2a9e83a6d3926f45a2daa58074
Carlo Landmeter <clandmeter@alpinelinux.org>
2019-02-15 10:33:10 +0000

    scripts: add modloopfw support
    this is needed for firware which is not automatically detected.

--- scripts/mkimg.arm.sh
+++ scripts/mkimg.arm.sh
@@ -75,6 +75,7 @@ profile_rpi() {
 	kernel_cmdline="dwc_otg.lpm_enable=0 console=tty1"
 	initfs_features="base bootchart squashfs ext4 f2fs kms mmc raid scsi usb"
 	hostname="rpi"
+	modloopfw="brcm/brcmfmac43455-sdio.clm_blob"
 }
 
 build_uboot() {
