ceb89c04caab32f8a5ea45afb975173a2aea146e
mterron <miguel.terron@gmail.com>
2019-03-15 16:09:51 +1300

    testing/nss_wrapper: new aport
    https://cwrap.org/nss_wrapper.html
    A wrapper for the user, group and hosts NSS API

---
+++ testing/nss_wrapper/nss.h
@@ -0,0 +1,13 @@
+#ifndef NSS__H
+#define NSS__H
+
+enum nss_status
+{
+		NSS_STATUS_TRYAGAIN = -2,
+		NSS_STATUS_UNAVAIL = -1,
+		NSS_STATUS_NOTFOUND = 0,
+		NSS_STATUS_SUCCESS = 1,
+		NSS_STATUS_RETURN = 2
+};
+
+#endif
