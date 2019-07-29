5c1708b014cc3eddfc091416ae33d7adf7c7a033
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2019-07-28 23:00:27 -0400

    Adding APKBUILD and its pioneer dependencies

---
+++ testing/pioneer/buildopts.h
@@ -0,0 +1,11 @@
+#ifndef BUILDOPTS_H
+#define BUILDOPTS_H
+
+#define PIONEER_EXTRAVERSION ""
+#define PIONEER_VERSION "20190723"
+#define PIONEER_DATA_DIR "/usr/share/pioneer"
+
+#define WITH_OBJECTVIEWER 1
+#define WITH_DEVKEYS 1
+
+#endif /* BUILDOPTS_H */
