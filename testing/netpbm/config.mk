d3b64a698a28e89ad2e538e5f9273bc605998573
Tom Parker-Shemilt <palfrey@tevp.net>
2020-01-15 21:35:59 +0000

    testing/netpbm: new aport
    http://netpbm.sourceforge.net/
    A toolkit for manipulation of graphic images

---
+++ testing/netpbm/config.mk
@@ -0,0 +1,14 @@
+DEFAULT_TARGET = nonmerge
+NETPBMLIBTYPE = unixshared
+NETPBMLIBSUFFIX = so
+STATICLIB_TOO = Y
+LDRELOC = ld --reloc
+LINKER_CAN_DO_EXPLICIT_LIBRARY = Y
+LINKERISCOMPILER = Y
+CFLAGS_SHLIB += -fPIC
+TIFFLIB = -ltiff
+JPEGLIB = -ljpeg
+PNGLIB = -lpng
+ZLIB = -lz
+X11LIB = -lX11
+NETPBM_DOCURL = http://netpbm.sourceforge.net/doc/
