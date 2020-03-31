9ef108e910b25625d97a76fbcd631e92ccaf20a3
Ariadne Conill <ariadne@dereferenced.org>
2020-03-31 05:03:49 +0000

    main/libc-dev: MIPS: sgidefs wrapper: define _OABI32 and _NABI32 and _ABI64 if missing

--- main/libc-dev/sgidefs.h
+++ main/libc-dev/sgidefs.h
@@ -1,1 +1,13 @@
 #include <asm/sgidefs.h>
+
+#ifndef _OABI32
+# define _OABI32	_MIPS_SIM_ABI32
+#endif
+
+#ifndef _NABI32
+# define _NABI32	_MIPS_SIM_NABI32
+#endif
+
+#ifndef _ABI64
+# define _ABI64		_MIPS_SIM_ABI64
+#endif
