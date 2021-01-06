5fdf74a30ac125b6941596973346ed5779113fe8
Jakub Jirutka <jakub@jirutka.cz>
2021-01-07 00:41:51 +0100

    community/postgresql-tsearch-czech: change dict and cfg name, rewrite SQL script

--- community/postgresql-tsearch-czech/tsearch_czech_create.sql
+++ community/postgresql-tsearch-czech/tsearch_czech_create.sql
@@ -1,7 +1,40 @@
+----
 -- Configure Czech ispell dictionary for fulltext search.
 --
--- https://postgres.cz/wiki/Instalace_PostgreSQL#Instalace_Fulltextu
+-- If the dictionary and configuration already exists, it will do nothing.
+----
 
-CREATE TEXT SEARCH DICTIONARY cspell (template=ispell, dictfile = czech, afffile=czech, stopwords=czech);
-CREATE TEXT SEARCH CONFIGURATION cs (copy=english);
-ALTER TEXT SEARCH CONFIGURATION cs ALTER MAPPING FOR word, asciiword WITH cspell, simple;
+\set cfgname 'czech'
+\set dictname 'czech_ispell'
+\set namespace 'pg_catalog'
+
+\out /dev/null
+\timing off
+
+
+SET search_path = :"namespace";
+
+-- Create TS dictionary if not exists
+
+SELECT EXISTS (SELECT 1 FROM pg_ts_dict WHERE dictnamespace = :'namespace'::regnamespace AND dictname = :'dictname') AS has_dict;
+\gset
+
+\if :has_dict
+  \echo text search dictionary :"dictname" already exists in namespace :"namespace"
+\else
+  \echo creating text search dictionary :"dictname" in namespace :"namespace"
+  CREATE TEXT SEARCH DICTIONARY :"dictname" (template=ispell, dictfile = czech, afffile=czech, stopwords=czech);
+\endif
+
+-- Create TS configuration if not exists
+
+SELECT EXISTS (SELECT 1 FROM pg_ts_config WHERE cfgnamespace = :'namespace'::regnamespace AND cfgname = :'cfgname') AS has_config;
+\gset
+
+\if :has_config
+  \echo text search configuration :"cfgname" already exists in namespace :"namespace"
+\else
+  \echo creating text search configuration :"cfgname" in namespace :"namespace"
+  CREATE TEXT SEARCH CONFIGURATION :"cfgname" (copy=english);
+  ALTER TEXT SEARCH CONFIGURATION :"cfgname" ALTER MAPPING FOR word, asciiword WITH :"dictname", simple;
+\endif
