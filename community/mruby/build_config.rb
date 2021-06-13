ee289aa7ba4097695d2ccf98389585fe6043dbd5
Jakub Jirutka <jakub@jirutka.cz>
2021-06-13 15:13:01 +0200

    community/mruby: upgrade to 3.0.0

--- community/mruby/build_config.rb
+++ community/mruby/build_config.rb
@@ -1,18 +1,20 @@
 MRuby::Build.new do |conf|
-  toolchain :gcc
+  conf.toolchain :gcc
 
   # include the default GEMs
   conf.gembox 'default'
 end
 
 MRuby::Build.new('host-debug') do |conf|
-  toolchain :gcc
+  conf.toolchain :gcc
+
+  conf.enable_debug
 
   # include the default GEMs
   conf.gembox 'default'
 
   # C compiler settings
-  conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
+  conf.cc.defines = %w(MRB_USE_DEBUG_HOOK)
 
   # Generate mruby debugger command (require mruby-eval)
   conf.gem :core => "mruby-bin-debugger"
