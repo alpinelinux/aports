c9a8a1897a45e94ed08b59062ca1ca074bf208cd
Alex Xu (Hello71) <alex_y_xu@yahoo.ca>
2021-08-09 18:52:28 -0400

    scripts/mkimg.base.sh: add openntpd to base
    fixes #10080

--- scripts/mkimg.base.sh
+++ scripts/mkimg.base.sh
@@ -320,7 +320,7 @@ profile_base() {
 	case "$ARCH" in
 	x86*) grub_mod="$grub_mod multiboot2 efi_uga";;
 	esac
-	apks="alpine-base busybox kbd-bkeymaps chrony dhcpcd e2fsprogs haveged network-extras openssl openssh tzdata wget"
+	apks="alpine-base busybox kbd-bkeymaps chrony dhcpcd e2fsprogs haveged network-extras openntpd openssl openssh tzdata wget"
 	apkovl=
 	hostname="alpine"
 }
