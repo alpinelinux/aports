0f98033c054d4632ba02392d6edb8b427ce35b70
Clayton Craft <clayton@craftyguy.net>
2021-08-23 21:53:14 -0700

    community/pipewire: fix launcher to only start installed components
    Alpine is using pipewire.conf to start components, but this is
    apparently not supported upstream. And it leads to strange issues when
    the component doesn't actually exist:
    https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/1527)
    
    The launcher will now run pipewire-media-session after starting
    pipewire, and *only* start pipewire-pulse if the config exists for it..
    rather than use pipewire to spawn child processes (which isn't supported
    upstream...).
    
    This fixes the following two issues:
    
    - An issue where pipewire starts a defunct process when trying to start
      pipewire-media-session if that binary doesn't exist, which in turn
      causes pipewire to ignore signals to stop on shutdown (makes shutdown
      take 90 seconds longer)
    
    - An issue where pipewire is started with -c pipewire-pulse.conf even if
      that config doesn't exist, and is lead suspect in a number of
      audio-related issues we are experiencing in pmOS

--- community/pipewire/pipewire-launcher.sh
+++ community/pipewire/pipewire-launcher.sh
@@ -2,5 +2,16 @@
 
 # We need to kill any existing pipewire instance to restore sound
 pkill -u "${USER}" -x pipewire 1>/dev/null 2>&1
+pkill -u "${USER}" -fx "pipewire -c pipewire-pulse.conf" 1>/dev/null 2>&1
+pkill -u "${USER}" -fx /usr/bin/pipewire-media-session 1>/dev/null 2>&1
 
-exec /usr/bin/pipewire
+exec /usr/bin/pipewire &
+
+# wait for pipewire to start before attempting to start related daemons
+while [ "$(pgrep -f /usr/bin/pipewire)" = "" ]; do
+        sleep 1
+done
+
+[ -x /usr/bin/pipewire-media-session ]  && exec /usr/bin/pipewire-media-session &
+
+[ -f "/usr/share/pipewire/pipewire-pulse.conf" ] && exec pipewire -c pipewire-pulse.conf &
