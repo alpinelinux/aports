0ab53fe0620f9b8ee0e7ee321be3f9b77ff4c913
Thomas Kienlen <kommander@laposte.net>
2021-11-12 22:07:16 +0100

    testing/flightgear: upgrade to 2021.3.11

---
+++ testing/flightgear/flightgear-data-downloader.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+FGDATA_VERSION=2020.3.11
+RELEASE_BRANCH=${FGDATA_VERSION%.*}
+
+echo "Downloading Flightgear data (version $FGDATA_VERSION) ..."
+curl -L https://sourceforge.net/projects/flightgear/files/release-$RELEASE_BRANCH/FlightGear-$FGDATA_VERSION-data.txz/download | tar -xJ --strip-components=1 -C /var/lib/flightgear -f -
+echo 'Done.'
