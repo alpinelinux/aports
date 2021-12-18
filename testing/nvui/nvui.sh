9cce300bc5a742ca5382f2a0fbcbf69cabf5e87a
Jakub Jirutka <jakub@jirutka.cz>
2021-12-18 02:56:47 +0100

    testing/nvui: fix locale::facet::_S_create_c_locale name not valid
    LANG=en_US.UTF8 nor LANG=C.UTF8 doesn't work. :/

--- testing/nvui/nvui.sh
+++ testing/nvui/nvui.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec /usr/libexec/nvui --detached -- "$@"
+# XXX: Set LANG=C to avoid error locale::facet::_S_create_c_locale name not valid.
+LANG=C exec /usr/libexec/nvui --detached -- "$@"
