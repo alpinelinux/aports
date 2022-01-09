4c7dd8517662077c8d2faa640ed00838f15a6e83
ktprograms <ktprograms@gmail.com>
2022-01-09 13:58:10 +0800

    testing/flightgear: run mkdir in data downloader
    Otherwise tar can't change into /var/lib/flightgear, which causes curl
    to have error 23

--- testing/flightgear/flightgear-data-downloader.sh
+++ testing/flightgear/flightgear-data-downloader.sh
@@ -4,5 +4,6 @@ FGDATA_VERSION=2020.3.11
 RELEASE_BRANCH=${FGDATA_VERSION%.*}
 
 echo "Downloading Flightgear data (version $FGDATA_VERSION) ..."
+mkdir /var/lib/flightgear
 curl -L https://sourceforge.net/projects/flightgear/files/release-$RELEASE_BRANCH/FlightGear-$FGDATA_VERSION-data.txz/download | tar -xJ --strip-components=1 -C /var/lib/flightgear -f -
 echo 'Done.'
