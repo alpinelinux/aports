d5633f50ea6b47d82e9336cead88ed19a0fdc7e4
Drew DeVault <sir@cmpwn.com>
2022-04-23 16:59:16 +0200

    community/hare: new aport

---
+++ community/hare/config.x86_64.mk
@@ -0,0 +1,31 @@
+## Install configuration
+
+PREFIX=/usr
+BINDIR=$(PREFIX)/bin
+MANDIR=$(PREFIX)/share/man
+SRCDIR=$(PREFIX)/src
+
+# Where to install the stdlib tree
+STDLIB=$(SRCDIR)/hare/stdlib
+
+# Default HAREPATH
+LOCALSRCDIR=/usr/src/hare
+HAREPATH=$(LOCALSRCDIR)/stdlib:$(LOCALSRCDIR)/third-party:$(SRCDIR)/hare/stdlib:$(SRCDIR)/hare/third-party
+
+## Build configuration
+
+# Platform to build for
+PLATFORM=linux
+ARCH=x86_64
+
+# External tools and flags
+HAREC=harec
+HAREFLAGS=
+QBE=qbe
+AS=as
+LD=ld
+AR=ar
+SCDOC=scdoc
+
+# Where to store build artifacts
+HARECACHE=.cache
