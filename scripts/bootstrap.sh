8c41723185ef37b1076692fa73f8913a9475b218
ptrcnull <git@ptrcnull.me>
2022-07-26 23:36:41 +0200

    scripts/bootstrap.sh: print usage on missing params before trying to use them
    fixes #10607

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,32 +8,6 @@ SUDO_APK=abuild-apk
 # optional cross build packages
 : ${KERNEL_PKG=linux-firmware linux-lts}
 
-# get abuild configurables
-[ -e /usr/share/abuild/functions.sh ] || (echo "abuild not found" ; exit 1)
-CBUILDROOT="$(CTARGET=$TARGET_ARCH . /usr/share/abuild/functions.sh ; echo $CBUILDROOT)"
-. /usr/share/abuild/functions.sh
-[ -z "$CBUILD_ARCH" ] && die "abuild is too old (use 2.29.0 or later)"
-[ -z "$CBUILDROOT" ] && die "CBUILDROOT not set for $TARGET_ARCH"
-export CBUILD
-
-# deduce aports directory
-[ -z "$APORTS" ] && APORTS=$(realpath $(dirname $0)/../)
-[ -e "$APORTS/main/build-base" ] || die "Unable to deduce aports base checkout"
-
-apkbuildname() {
-	local repo="${1%%/*}"
-	local pkg="${1##*/}"
-	[ "$repo" = "$1" ] && repo="main"
-	echo $APORTS/$repo/$pkg/APKBUILD
-}
-
-msg() {
-	[ -n "$quiet" ] && return 0
-	local prompt="$GREEN>>>${NORMAL}"
-	local name="${BLUE}bootstrap-${TARGET_ARCH}${NORMAL}"
-        printf "${prompt} ${name}: %s\n" "$1" >&2
-}
-
 if [ -z "$TARGET_ARCH" ]; then
 	program=$(basename $0)
 	cat <<EOF
@@ -60,6 +34,32 @@ and other packages in the bootstrap path.
 EOF
 	return 1
 fi
+
+# get abuild configurables
+[ -e /usr/share/abuild/functions.sh ] || (echo "abuild not found" ; exit 1)
+CBUILDROOT="$(CTARGET=$TARGET_ARCH . /usr/share/abuild/functions.sh ; echo $CBUILDROOT)"
+. /usr/share/abuild/functions.sh
+[ -z "$CBUILD_ARCH" ] && die "abuild is too old (use 2.29.0 or later)"
+[ -z "$CBUILDROOT" ] && die "CBUILDROOT not set for $TARGET_ARCH"
+export CBUILD
+
+# deduce aports directory
+[ -z "$APORTS" ] && APORTS=$(realpath $(dirname $0)/../)
+[ -e "$APORTS/main/build-base" ] || die "Unable to deduce aports base checkout"
+
+apkbuildname() {
+	local repo="${1%%/*}"
+	local pkg="${1##*/}"
+	[ "$repo" = "$1" ] && repo="main"
+	echo $APORTS/$repo/$pkg/APKBUILD
+}
+
+msg() {
+	[ -n "$quiet" ] && return 0
+	local prompt="$GREEN>>>${NORMAL}"
+	local name="${BLUE}bootstrap-${TARGET_ARCH}${NORMAL}"
+        printf "${prompt} ${name}: %s\n" "$1" >&2
+}
 
 if [ ! -d "$CBUILDROOT" ]; then
 	msg "Creating sysroot in $CBUILDROOT"
