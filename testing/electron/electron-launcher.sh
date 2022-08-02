0083b78b6fe741021d6f1af3890e029e1d342088
Lauren N. Liberda <lauren@selfisekai.rocks>
2022-08-02 09:38:47 +0200

    testing/electron: add flag configs, run on wayland if possible

---
+++ testing/electron/electron-launcher.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Allow the user to override command-line flags
+# This is based on Debian's chromium-browser package, and is intended
+# to be consistent with Debian.
+for f in /etc/electron/*.conf; do
+    [ -f ${f} ] && . "${f}"
+done
+
+# Prefer user defined ELECTRON_USER_FLAGS (from env) over system
+# default ELECTRON_FLAGS (from /etc/electron/default.conf).
+ELECTRON_FLAGS=${ELECTRON_USER_FLAGS:-"$ELECTRON_FLAGS"}
+
+exec "/usr/lib/electron/electron" ${ELECTRON_FLAGS} "$@"
