2ad9d07894722d78e90969c71561cc08e9bf1d16
psykose <alice@ayaya.dev>
2022-08-20 07:23:57 +0000

    scripts/bootstrap.sh: move ghc after llvm14

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -125,10 +125,10 @@ for PKG in fortify-headers linux-headers musl libc-dev pkgconf zlib \
 	   lzip abuild ncurses libedit openssh \
 	   libcap-ng util-linux libaio lvm2 popt xz \
 	   json-c argon2 cryptsetup zstd kmod lddtree mkinitfs \
-	   community/go libffi community/ghc \
+	   community/go libffi \
 	   brotli libev c-ares cunit nghttp2 curl \
 	   pcre libssh2 \
-	   libxml2 pax-utils llvm14 community/rust \
+	   libxml2 pax-utils llvm14 community/ghc community/rust \
 	   $KERNEL_PKG ; do
 
 	if [ "$NEEDS_LIBATOMIC" = "yes" ]; then
