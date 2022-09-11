e7b03581dc629fb3207e45e5d0a672f454fc13a6
Jakub Jirutka <jakub@jirutka.cz>
2022-09-11 23:17:17 +0200

    community/acpid: remove zzz from depends, make it optional
    Resolves #14165

--- community/acpid/handler.sh
+++ community/acpid/handler.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-# vim: set ts=4:
+# vim: set ts=4 sw=4:
 #
 # This is the default ACPI handler script that is configured in
 # /etc/acpi/events/anything to be called for every ACPI event.
 # You can edit it and add your own actions; treat it as a configuration file.
+#
+# lid-closed, power-supply-ac and suspend are scripts located in
+# /usr/share/acpid/.
 #
 PATH="/usr/share/acpid:$PATH"
 alias log='logger -t acpid'
@@ -15,25 +18,24 @@ button/power:PWRF:* | button/power:PBTN:*)
 	log 'Power button pressed'
 	# If we have a lid (notebook), suspend to RAM, otherwise power off.
 	if [ -e /proc/acpi/button/lid/LID ]; then
-		zzz
+		suspend
 	else
 		poweroff
 	fi
 ;;
 button/sleep:SLPB:*)
 	log 'Sleep button pressed'
-	# Suspend to RAM.
-	zzz
+	suspend
 ;;
 button/lid:*:close:*)
 	log 'Lid closed'
 	# Suspend to RAM if AC adapter is not connected.
-	power-supply-ac || zzz
+	power-supply-ac || suspend
 ;;
 ac_adapter:*:*:*0)
 	log 'AC adapter unplugged'
 	# Suspend to RAM if notebook's lid is closed.
-	lid-closed && zzz
+	lid-closed && suspend
 ;;
 esac
 
