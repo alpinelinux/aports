70adf958666f25fdbfed7dbb9e01c3c8436ef5f8
Natanael Copa <ncopa@alpinelinux.org>
2022-11-18 22:57:49 +0100

    scripts/mkimg.base.sh: fix typos
    fixes commit dc0db48c5a32 (scripts/mkimg.base.sh: include doas in base)

--- scripts/mkimg.base.sh
+++ scripts/mkimg.base.sh
@@ -323,7 +323,7 @@ profile_base() {
 	x86*) grub_mod="$grub_mod multiboot2 efi_uga";;
 	esac
 	apks="alpine-base busybox chrony dhcpcd doas e2fsprogs haveged
-		kbd-bkeymap snetwork-extras openntpd openssl openssh
+		kbd-bkeymaps network-extras openntpd openssl openssh
 		tzdata wget"
 	apkovl=
 	hostname="alpine"
