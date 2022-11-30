3a5853981a7c2510f48edaeaf7ae9a9f68bef7a5
Kevin Daudt <kdaudt@alpinelinux.org>
2022-11-30 21:11:35 +0000

    community/netdata: upgrade to 1.37.0

--- community/netdata/submodule_commits.sh
+++ community/netdata/submodule_commits.sh
@@ -31,7 +31,7 @@ websockets_submodules="$(req underhood/mqtt_websockets/git/trees/"${tree}"\?recu
 
 printf "_aclk_schemas_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit aclk/aclk-schemas)"
 printf "_ml_json_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit ml/json)"
-printf "_ml_kmeans_dlib_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit ml/kmeans/dlib)"
+printf "_ml_dlib_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit ml/dlib)"
 printf "_mqtt_websockets_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit mqtt_websockets)"
 printf "_mqtt_c_commit=%s\n" "$(echo "$websockets_submodules" | submodule_commit MQTT-C)"
 printf "_c_rbuf_commit=%s\n" "$(echo "$websockets_submodules" | submodule_commit c-rbuf)"
