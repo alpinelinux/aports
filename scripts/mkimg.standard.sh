b0b993207b5c84f3b20e81ce1e7df216d3e867fd
psykose <alice@ayaya.dev>
2023-02-13 13:59:42 +0000

    scripts/mkimg.standard.sh: add lz4/xz/zstd to extended
    requested on irc- this makes decompressing data more convenient
    starting only from an extended iso in certain usecases of having
    additional images during installation.
    
    increases image size by ~1.7MB

--- scripts/mkimg.standard.sh
+++ scripts/mkimg.standard.sh
@@ -57,8 +57,8 @@ profile_extended() {
 
 		btrfs-progs cksfv dosfstools cryptsetup
 		e2fsprogs e2fsprogs-extra efibootmgr f2fs-tools
-		grub-bios grub-efi lvm2 mdadm mkinitfs mtools nfs-utils
-		parted rsync sfdisk syslinux util-linux xfsprogs zfs
+		grub-bios grub-efi lvm2 lz4 mdadm mkinitfs mtools nfs-utils
+		parted rsync sfdisk syslinux util-linux xfsprogs zstd zfs
 		"
 
 	local _k _a
