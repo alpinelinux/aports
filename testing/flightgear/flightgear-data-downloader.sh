7e5dc9e552a91d73b10f25931aa2cf938fc65250
Thomas Kienlen <kommander@laposte.net>
2023-03-06 10:31:08 +0000

    testing/flightgear: fix data download script

--- testing/flightgear/flightgear-data-downloader.sh
+++ testing/flightgear/flightgear-data-downloader.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-FGDATA_VERSION=2020.3.11
+FGDATA_VERSION=%FGDATA_VERSION%
 RELEASE_BRANCH=${FGDATA_VERSION%.*}
 
+if [ "$(id -u)" != '0' ]; then
+  echo "This script requires root privileges to store flightgear-data into /var/lib/flightgear folder."
+  exit 1
+fi
+
 echo "Downloading Flightgear data (version $FGDATA_VERSION) ..."
+rm -rf /var/lib/flightgear
 mkdir /var/lib/flightgear
 curl -L https://sourceforge.net/projects/flightgear/files/release-$RELEASE_BRANCH/FlightGear-$FGDATA_VERSION-data.txz/download | tar -xJ --strip-components=1 -C /var/lib/flightgear -f -
 echo 'Done.'
