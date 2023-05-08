2894793aa2182ef7c9984412e0cb20a1381c8c01
Natanael Copa <ncopa@alpinelinux.org>
2023-05-08 22:53:16 +0200

    main/mkinitfs: use tiny-cloud-alpine instead of nocloud
    ref https://gitlab.alpinelinux.org/alpine/cloud/tiny-cloud/-/merge_requests/54#note_305672

--- scripts/mkimg.base.sh
+++ scripts/mkimg.base.sh
@@ -327,7 +327,7 @@ profile_base() {
 	esac
 	apks="alpine-base busybox chrony dhcpcd doas e2fsprogs
 		kbd-bkeymaps network-extras openntpd openssl openssh
-		tzdata wget tiny-cloud-nocloud"
+		tzdata wget tiny-cloud-alpine"
 	apkovl=
 	hostname="alpine"
 }
