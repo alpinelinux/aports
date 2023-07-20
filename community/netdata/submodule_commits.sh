cf32d81a99fec4f2ac5016cd5d208255a507ca0f
Kevin Daudt <kdaudt@alpinelinux.org>
2023-07-20 20:05:19 +0000

    community/netdata: upgrade to 1.41.0

--- community/netdata/submodule_commits.sh
+++ community/netdata/submodule_commits.sh
@@ -19,19 +19,14 @@ submodule_commit() {
 	jq -r --arg path "$path" 'select(.path == $path) | .sha'
 }
 
-tag=$(req netdata/netdata/git/ref/tags/"${version}" | jq -r '.object.sha')
-commit=$(req netdata/netdata/git/tags/"${tag}" | jq -r '.object.sha')
-tree=$(req netdata/netdata/git/commits/"${commit}" | jq -r '.tree.sha')
-
-netdata_submodules="$(req netdata/netdata/git/trees/"${tree}"\?recursive=true | jq '.tree[] | select(.type == "commit")')"
+netdata_submodules="$(req netdata/netdata/git/trees/"${version}"\?recursive=true | jq '.tree[] | select(.type == "commit")')"
 
 websockets_commit="$(echo "$netdata_submodules" | submodule_commit mqtt_websockets)"
-tree=$(req underhood/mqtt_websockets/git/commits/"${websockets_commit}" | jq -r '.tree.sha')
-websockets_submodules="$(req underhood/mqtt_websockets/git/trees/"${tree}"\?recursive=true | jq '.tree[] | select(.type == "commit")')"
+websockets_submodules="$(req underhood/mqtt_websockets/git/trees/"${websockets_commit}"\?recursive=true | jq '.tree[] | select(.type == "commit")')"
 
 printf "_aclk_schemas_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit aclk/aclk-schemas)"
 printf "_ml_dlib_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit ml/dlib)"
 printf "_mqtt_websockets_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit mqtt_websockets)"
-printf "_h2o_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit httpd/h2o)"
+printf "_h2o_commit=%s\n" "$(echo "$netdata_submodules" | submodule_commit web/server/h2o/libh2o)"
 printf "_c_rbuf_commit=%s\n" "$(echo "$websockets_submodules" | submodule_commit c-rbuf)"
 printf "_c_rhash_commit=%s\n" "$(echo "$websockets_submodules" | submodule_commit c_rhash)"
