d351d2eb127d96d4bf0ab1c85a2732c1d1095b73
Celeste <20312-Celeste@users.gitlab.alpinelinux.org>
2023-11-30 07:39:48 +0000

    testing/perl-javascript-quickjs: new aport
    https://metacpan.org/release/JavaScript-QuickJS/
    Run JavaScript via QuickJS in Perl

---
+++ testing/perl-javascript-quickjs/quickjs-shared.mk.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use ExtUtils::MakeMaker::CPANfile;
+
+use Config;
+
+use File::Temp;
+use File::Slurper;
+use File::Spec;
+use File::Which;
+use Cwd;
+
+WriteMakefile(
+    NAME              => 'JavaScript::QuickJS',
+    VERSION_FROM      => 'lib/JavaScript/QuickJS.pm', # finds $VERSION
+    ($] >= 5.005 ?     ## Add these new keywords supported since 5.005
+      (ABSTRACT_FROM  => 'lib/JavaScript/QuickJS.pm', # retrieve abstract from module
+       AUTHOR         => [
+            'Felipe Gasper (FELIPE)',
+        ],
+      ) : ()
+    ),
+    INC               => '-Wall --std=c99 -I.',
+    LICENSE           => "perl_5",
+
+    PMLIBDIRS => ['lib'],
+
+    LIBS => "-lm -ldl -lpthread -lquickjs",
+
+    META_MERGE => {
+        'meta-spec' => { version => 2 },
+        resources => {
+            repository => {
+                type => 'git',
+                url => 'git://github.com/FGasper/p5-JavaScript-QuickJS.git',
+                web => 'https://github.com/FGasper/p5-JavaScript-QuickJS',
+            },
+            bugtracker => {
+                web => 'https://github.com/FGasper/p5-JavaScript-QuickJS/issues',
+            },
+        },
+    },
+);
+
+1;
