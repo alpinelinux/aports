f9fa2c25ae24f7651f2ca81a39ff1ef0af76d3d6
Sertonix <sertonix@posteo.net>
2023-12-22 15:00:11 +0100

    testing/librewolf: upgrade to 121.0-1

--- testing/librewolf/vendor-prefs.js
+++ testing/librewolf/vendor-prefs.js
@@ -4,12 +4,6 @@ pref("intl.locale.requested", "");
 // Use system-provided dictionaries
 pref("spellchecker.dictionary_path", "/usr/share/hunspell");
 
-// Disable default browser checking.
-pref("browser.shell.checkDefaultBrowser", false);
-
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
-
-// Disable sponsored tiles from "Mozilla Tiles Service"
-pref("browser.topsites.contile.enabled", false);
