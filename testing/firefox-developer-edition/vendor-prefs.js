40fe97ecb1314d20821fba8f43a72e8bf0b62c09
ptrcnull <git@ptrcnull.me>
2024-01-05 04:22:18 +0100

    testing/firefox-developer-edition: disable sponsored tiles and Widevine CDM

--- testing/firefox-developer-edition/vendor-prefs.js
+++ testing/firefox-developer-edition/vendor-prefs.js
@@ -10,3 +10,10 @@ pref("browser.shell.checkDefaultBrowser", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
+
+// Disable sponsored tiles from "Mozilla Tiles Service"
+pref("browser.topsites.contile.enabled", false);
+
+// Disable Widevine CDM support (doesn't work on musl anyway)
+pref("media.gmp-widevinecdm.visible", false);
+pref("media.gmp-widevinecdm.enabled", false);
