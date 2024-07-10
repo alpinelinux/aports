32dbf78e1cc2778882d6e0f210f11a285ae86260
Henrik Grimler <henrik@grimler.se>
2024-07-10 21:54:11 +0200

    community/heimdall: deprecate heimdall_flash_kernel.sh
    Replace the script with a heimdall_flash_isorec_kernel.sh script,
    which takes a fifth arg for path to device dtb, and concatenates and
    flashes vmlinuz and the dtb.

--- community/heimdall/heimdall_flash_isorec_kernel.sh
+++ community/heimdall/heimdall_flash_isorec_kernel.sh
@@ -7,28 +7,28 @@ usage()
 	echo "The kernel needs to have its own minimal initramfs, that loads the"
 	echo "real initramfs from the other partition (\"isorec\")."
 	echo ""
-	echo "Usage: $(basename "$0") <initfs> <initfs partition> <kernel> <kernel partition>"
+	echo "Usage: $(basename "$0") <initfs> <initfs partition> <kernel> <kernel partition> <dtb>"
 	exit 1
 }
 
 # Sanity checks
-[ "$#" != 4 ] && usage
+[ "$#" != 5 ] && usage
 INITFS="$1"
 INITFS_PARTITION="$2"
 KERNEL="$3"
 KERNEL_PARTITION="$4"
+DTB="$5"
+
 for file in "$INITFS" "$KERNEL"; do
 	[ -e "$file" ] && continue
 	echo "ERROR: File $file does not exist!"
 	exit 1
 done
 
-echo "DEPRECATION WARNING: heimdall_flash_kernel.sh is deprecated and will be"
-echo "removed in a future version. Please use heimdall_flash_isorec_kernel.sh"
-echo "instead."
 echo "Flash initramfs to the '$INITFS_PARTITION' partition (isorec-style) and"
 echo "kernel to the '$KERNEL_PARTITION' partition"
-heimdall_wait_for_device.sh
 gunzip -c "$INITFS" | lzop > /tmp/initramfs.lzo
-heimdall flash --"$INITFS_PARTITION" /tmp/initramfs.lzo --"$KERNEL_PARTITION" "$KERNEL"
+cat "$KERNEL" "$DTB" > /tmp/vmlinuz-dtb
+heimdall flash --"$INITFS_PARTITION" /tmp/initramfs.lzo --"$KERNEL_PARTITION" /tmp/vmlinuz-dtb
 rm /tmp/initramfs.lzo
+rm /tmp/vmlinuz-dtb
