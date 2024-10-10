e77508ef091a3ce4c08d459e24e0dee66b030b3d
ptrcnull <git@ptrcnull.me>
2024-10-10 18:11:36 +0200

    community/firefox-esr: upgrade to 128.3.1

--- community/firefox-esr/vendor-prefs.js
+++ community/firefox-esr/vendor-prefs.js
@@ -10,3 +10,13 @@ pref("browser.shell.checkDefaultBrowser", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
+
+// Disable sponsored tiles from "Mozilla Tiles Service"
+pref("browser.topsites.contile.enabled", false);
+
+// Disable Widevine CDM support (doesn't work on musl anyway)
+pref("media.gmp-widevinecdm.visible", false);
+pref("media.gmp-widevinecdm.enabled", false);
+
+// Disable Privacy-Preserving Attribution Measurement
+pref("dom.private-attribution.submission.enabled", false);
