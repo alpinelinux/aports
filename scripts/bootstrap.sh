df1f7e6fa5a2837a738b9f6eb22c556f34b52839
J. Neuschäfer <j.ne@posteo.net>
2025-06-15 01:11:40 +0000

    scripts/bootstrap.sh: move pax-utils to main list
    scanelf (part of pax-utils) is a dependency of musl-utils, which is
    installed in the "minirootfs" profile of mkimage.sh. So, it makes sense to
    build pax-utils as part of a regular bootstrap.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,9 +15,9 @@ shift
 
 # optional cross build packages
 #: ${KERNEL_PKG=linux-firmware linux-lts}
-#: ${COMPILER_PKG=libffi brotli libev c-ares cunit nghttp2 libidn2 libunistring libpsl curl libssh2 libxml2 pax-utils llvm15 community/ghc llvm20 rust community/go}
+#: ${COMPILER_PKG=libffi brotli libev c-ares cunit nghttp2 libidn2 libunistring libpsl curl libssh2 libxml2 llvm15 community/ghc llvm20 rust community/go}
 # FIXME: Maybe subdivide into ghc, rust, and go stuff
-: ${MKINITFS=libcap-ng ncurses readline sqlite util-linux libaio lvm2 popt xz json-c argon2 cryptsetup kmod lddtree mkinitfs}
+: ${MKINITFS=libcap-ng ncurses readline sqlite util-linux libaio lvm2 popt xz json-c argon2 cryptsetup kmod lddtree mkinitfs pax-utils}
 #: ${OPENSSH=openssh}
 
 if [ -z "$TARGET_ARCH" ]; then
