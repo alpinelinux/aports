6779e192ee81c5e7220a92969a799eca2177053a
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2025-09-26 22:35:23 +0200

    testing/signal-desktop: pass arguments
    Pass arguments passed to the wrapper script to signal itself.
    
    This allows appending flags such as: --enable-features=UseOzonePlatform --ozone-platform=wayland

--- testing/signal-desktop/signal-desktop.sh
+++ testing/signal-desktop/signal-desktop.sh
@@ -3,4 +3,4 @@
 # app chooses config (including used endpoints) based on this
 export NODE_ENV=production
 
-exec electron /usr/lib/signal-desktop/app.asar
+exec electron /usr/lib/signal-desktop/app.asar "$@"
