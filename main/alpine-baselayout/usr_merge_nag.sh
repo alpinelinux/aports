35e28f075376cd07dbb323b5c5fe832b5f313797
Ariadne Conill <ariadne@ariadne.space>
2025-10-17 11:07:26 -0700

    main/alpine-baselayout: split usr-merge-nag warning into a subpackage

--- main/alpine-baselayout/usr_merge_nag.sh
+++ main/alpine-baselayout/usr_merge_nag.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 if [ ! -L /lib ] && [ "$1" = "post-commit" ]; then
 	cat << __EOF__
-* WARNING: The current system is not /usr-merged. You are encouraged to
-* migrate manually to ensure the best-possible support. See
-* https://alpinelinux.org/posts/2025-10-01-usr-merge.html for more details
+* WARNING: The current system is not /usr-merged.
+* You are encouraged to migrate manually to ensure system reliability.
+*
+* See https://alpinelinux.org/posts/2025-10-01-usr-merge.html for more
+* details.
+*
+* You can disable this message by masking the usr-merge-nag package:
+*   apk add '!usr-merge-nag'
 __EOF__
 fi
