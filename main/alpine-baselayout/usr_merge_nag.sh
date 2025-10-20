859a6644da60a12f8912a5289c0b81885904cfaa
Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
2025-10-20 18:07:25 +0200

    main/alpine-baselayout: upgrade usr-nag-script with opt-in and timeline

--- main/alpine-baselayout/usr_merge_nag.sh
+++ main/alpine-baselayout/usr_merge_nag.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 if [ ! -L /lib ] && [ "$1" = "post-commit" ]; then
 	cat << __EOF__
-* WARNING: The current system is not /usr-merged.
-* You are encouraged to migrate manually to ensure system reliability.
+* INFO: The current system is not /usr-merged.
+* In the future, the /usr-merge will be compulsory and the migration will happen
+* automatically. You can help testing the migration path by running it manually
+* before, but there might be known and unknown bugs at the time of migration:
+* https://gitlab.alpinelinux.org/groups/alpine/-/milestones/16
 *
-* See https://alpinelinux.org/posts/2025-10-01-usr-merge.html for more
-* details.
+* See https://alpinelinux.org/posts/2025-10-01-usr-merge.html and
+* https://gitlab.alpinelinux.org/alpine/aports/-/issues/17624#note_551336
+* for more details.
 *
 * You can disable this message by masking the usr-merge-nag package:
 *   apk add '!usr-merge-nag'
