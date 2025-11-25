8c91eaed48faaf55833e0725a52ab8d4d364db94
Achill Gilgenast <achill@achill.org>
2025-11-25 15:08:19 +0100

    main/alpine-baselayout: remove prefix of usr_merge_nag.sh

--- main/alpine-baselayout/usr_merge_nag.sh
+++ main/alpine-baselayout/usr_merge_nag.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 if [ ! -L /lib ] && [ "$1" = "post-commit" ]; then
 	cat << __EOF__
-* INFO: The current system is not /usr-merged.
-* In the future, the /usr-merge will be compulsory and the migration will happen
-* automatically. You can help testing the migration path by running it manually
-* before, but there might be known and unknown bugs at the time of migration:
-* https://gitlab.alpinelinux.org/groups/alpine/-/milestones/16
-*
-* See https://alpinelinux.org/posts/2025-10-01-usr-merge.html and
-* https://gitlab.alpinelinux.org/alpine/aports/-/issues/17624#note_551336
-* for more details.
-*
-* You can disable this message by masking the usr-merge-nag package:
-*   apk add '!usr-merge-nag'
+INFO: The current system is not /usr-merged.
+In the future, the /usr-merge will be compulsory and the migration will happen
+automatically. You can help testing the migration path by running it manually
+before, but there might be known and unknown bugs at the time of migration:
+https://gitlab.alpinelinux.org/groups/alpine/-/milestones/16
+
+See https://alpinelinux.org/posts/2025-10-01-usr-merge.html and
+https://gitlab.alpinelinux.org/alpine/aports/-/issues/17624#note_551336
+for more details.
+
+You can disable this message by masking the usr-merge-nag package:
+  apk add '!usr-merge-nag'
 __EOF__
 fi
