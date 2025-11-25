66538dc4ff2e3c0d824855ca79c5cdca4a22a658
Natanael Copa <ncopa@alpinelinux.org>
2025-11-25 14:06:14 +0100

    Revert "scripts/mkimg.base.sh: add link about old bug on old macs"
    It does not boot.
    
    This reverts commit d98bdf4e318928a75f49f2ed7934cd9f45b5c3d1.
    This reverts commit 37ee7b773682280f3e55157b95ed4a6302638155.

--- scripts/mkimg.base.sh
+++ scripts/mkimg.base.sh
@@ -261,9 +261,6 @@ create_image_iso() {
 	if [ -e "${DESTDIR}/efi" -a -e "${DESTDIR}/boot/grub" ]; then
 		# Create the EFI boot partition image
 		mformat -i ${DESTDIR}/boot/grub/efi.img -C -f 1440 -N 0 ::
-		# work around bug on old macs
-		# https://www.mail-archive.com/grub-devel@gnu.org/msg28931.html
-		dd if=/dev/zero of=${DESTDIR}/boot/grub/efi.img bs=1 seek=446 count=16
 		mcopy -i ${DESTDIR}/boot/grub/efi.img -s ${DESTDIR}/efi ::
 		touch -md "@${SOURCE_DATE_EPOCH}" ${DESTDIR}/boot/grub/efi.img
 
