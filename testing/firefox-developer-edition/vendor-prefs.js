3a7ddc50632df822689e39cde4bb5181016dfddc
ptrcnull <git@ptrcnull.me>
2026-05-27 05:11:15 +0200

    testing/firefox-developer-edition: upgrade to 152.0_beta4

--- testing/firefox-developer-edition/vendor-prefs.js
+++ testing/firefox-developer-edition/vendor-prefs.js
@@ -16,9 +16,18 @@ pref("browser.topsites.contile.enabled", false);
 pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 
-// Disable Widevine CDM support (doesn't work on musl anyway)
+// Disable Widevine CDM support (unusable error message when gcompat is missing)
 pref("media.gmp-widevinecdm.visible", false);
 pref("media.gmp-widevinecdm.enabled", false);
 
 // Disable Privacy-Preserving Attribution Measurement
 pref("dom.private-attribution.submission.enabled", false);
+
+// Disable various telemetry sources
+pref("browser.newtabpage.activity-stream.telemetry", false);
+pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+pref("toolkit.telemetry.newProfilePing.enabled", false);
+pref("toolkit.telemetry.updatePing.enabled", false);
+pref("toolkit.telemetry.bhrPing.enabled", false);
+pref("datareporting.healthreport.uploadEnabled", false);
+pref("datareporting.usage.uploadEnabled", false);
